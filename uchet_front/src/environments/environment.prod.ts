86f4000970e4c5f72b8df6f11c48e03e6e26f235
Server <server.ares@gmail.com>
2019-09-17 20:58:22 +0300

    Dist prod and fix

--- uchet_front/src/environments/environment.prod.ts
+++ uchet_front/src/environments/environment.prod.ts
@@ -11,6 +11,6 @@ export const environment = {
   },
   gfClientId: '177386660212-5fo4hhak9eo35qilvpg4ittbsh5h9m81.apps.googleusercontent.com',
   gfSecret: 'eSxkXW4EJM6KIyEgaUCM-a_2',
-  apiUrl: 'http://uchet.part4.info:50101/api/',
-  host: 'http://uchet.part4.info:50101/'
+  apiUrl: 'http://services.part4.info:50101/api/',
+  host: 'http://services.part4.info:50101/api/'
 };
